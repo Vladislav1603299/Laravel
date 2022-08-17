806403efedb818c4fd4b27b0accb609bba15a9f4
lesson1 <abinin99@bk.ru>
2022-08-18 00:39:59 +0300

    add homework 3

--- first/resources/views/news/show.php
+++
@@ -1,8 +0,0 @@
-<div style="border: 1px solid green; margin-top: 30px;">
-    <h2><?= $news['title'] ?></h2>
-    <p><?= $news['autor'] ?> - <?= $news['created_at'] ?></p>
-    <p><?= $news['description'] ?></p>
-</div>
-<div style="text-align:center; margin-top: 100px;">
-    <a href="/news/category/<?= $news['id'] ?>">Назад</a>
-</div>
